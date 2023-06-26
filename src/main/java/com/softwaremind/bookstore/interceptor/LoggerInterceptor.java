ac412df209b4cbd8c2d3cc3747a4fb7e4583ef09
dgacek <dawid.gacek451@outlook.com>
2023-06-26 18:45:53 +0200

    #9
    - Fixed warnings in LoggerInterceptor
    - Made that only /api/ requests are logged

--- src/main/java/com/softwaremind/bookstore/interceptor/LoggerInterceptor.java
+++ src/main/java/com/softwaremind/bookstore/interceptor/LoggerInterceptor.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.HandlerMapping;
@@ -16,8 +17,9 @@ public class LoggerInterceptor implements HandlerInterceptor {
     private final Marker marker = MarkerFactory.getMarker("API");
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info(marker, "{} {}", request.getMethod(), request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE));
+    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
+        if (request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString().startsWith("/api"))
+            logger.info(marker, "{} {}", request.getMethod(), request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE));
         return true;
     }
 }
